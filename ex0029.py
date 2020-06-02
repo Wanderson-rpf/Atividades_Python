933273fa34a1e047be7705eea499e6c6e0c1c524
Wanderson Ricardo <62866832+Wanderson-rpf@users.noreply.github.com>
2020-06-02 14:06:43 -0300

    result

--- ex0029.py
+++ ex0029.py
@@ -5,3 +5,16 @@ O nome com todas as letras minúsculas
 Quantas letras ao total (sem considerar espaços)
 Quantas letras tem o primeiro nome
 """
+print('=' * 40)
+nome = str(input('Informe o nome e sobre nome: ')).strip()
+print('=' * 40)
+
+print('RESUMO DE INFORMAÇÕES DO NOME'.center(40))
+print('=' * 40)
+
+print(f'- Nome com letras maiúsculas: {nome.upper()}')
+print(f'- Nome com letras minúsculas: {nome.lower()}')
+print(f'- O nome possui {len(nome) - nome.count(" ")} letras.')
+separar = nome.split()
+print(f'- O primeiro nome tem {len(separar[0])}')
+
--- ex0030.py
+++ ex0030.py
@@ -2,4 +2,16 @@
 Desenvolva um programa que leia o comprimento de tres retas e dica
 ao usuário se elas podem ou não formar um triangulo.
 """
+print('=' * 50)
+print('VERIFICAÇÃO DE RETA PARA TRIANGULO'.center(50))
+print('=' * 50)
 
+r1 = float(input('Informe o comprimento da 1° reta: '))
+r2 = float(input('Informe o comprimento da 2° reta: '))
+r3 = float(input('Informe o comprimento da 3° reta: '))
+print('=' * 50)
+
+if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
+    print('-> As retas formam um triangulo')
+else:
+    print('-> As retas não formam um triangulo.')
