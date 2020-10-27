43b190830028c2b0f635332b68356b13cf35b09a
Wanderson Ricardo <62866832+Wanderson-rpf@users.noreply.github.com>
2020-10-27 10:42:38 -0300

    .

--- ex0034.py
+++ ex0034.py
@@ -3,3 +3,14 @@ Faça um programa que leia 5 valores numericos e guarde em uma lista.
 no final mostre qual foi o maior e o menor valor digitado  as suas respectivas posições.
 
 """
+lista_num = []
+
+for i in range(1, 6):
+    num = int(input('Digite um valor inteiro: '))
+    lista_num.append(num)
+print('-' * 50)
+print(f'A lista de números foi: {lista_num}')
+print(f'O maior valor digitado foi {max(lista_num)} e esta na {lista_num.index(max(lista_num)) + 1}° posição.')
+print(f'O menor valor digitado foi {min(lista_num)} e esta na {lista_num.index(min(lista_num)) + 1}° posição.')
+print('-' * 50)
+
